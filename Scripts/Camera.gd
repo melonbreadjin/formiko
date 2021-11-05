a29762d680174e8ecd62459a71080a5128a6c450
yolstatrisch <y00ra@naver.com>
2021-11-06 01:18:44 +0800

    Fixed camera clamping

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -2,6 +2,12 @@ extends Camera2D
 
 var direction : Vector2
 
+func _ready():
+	limit_top = 0
+	limit_left = 0
+	limit_right = int(Globals.world_size.x * Globals.BLOCK_SIZE)
+	limit_bottom = int(Globals.world_size.y * Globals.BLOCK_SIZE)
+
 func _process(delta : float) -> void:
 	direction = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
@@ -10,5 +16,5 @@ func _process(delta : float) -> void:
 	direction = direction.normalized()
 	
 	position += direction * Globals.CAMERA_MOVESPEED * Globals.BLOCK_SIZE * delta
-	position.x = clamp(position.x, 0, Globals.world_size.x * Globals.BLOCK_SIZE)
-	position.y = clamp(position.y, 0, Globals.world_size.y * Globals.BLOCK_SIZE)
+	position.x = clamp(position.x, limit_left, limit_right - get_viewport().size.x)
+	position.y = clamp(position.y, limit_top, limit_bottom - get_viewport().size.y)
